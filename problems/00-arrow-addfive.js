5e23fbd7ff1443f36fc872ad9cb5ca3b56cb42b2
juniper <benklee127@gmail.com>
2022-12-02 16:08:41 -0500

    #00

--- problems/00-arrow-addfive.js
+++ problems/00-arrow-addfive.js
@@ -15,7 +15,18 @@ let result3 = addFive(37); // returns 42
 
 ***********************************************************************/
 
-// Your code here
+let addFive = num => {
+  return num + 5;
+}
+
+
+//Examples:
+let result1 = addFive(0); // returns 5
+console.log('result1');
+let result2 = addFive(10); // returns 15
+console.log('result2');
+let result3 = addFive(37); // returns 42
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
