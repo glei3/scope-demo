845423f9955a68b649ef98c24b0fc03c6e9c21aa
juniper <benklee127@gmail.com>
2022-12-02 17:29:52 -0500

    #08

--- problems/07-set-time-out.js
+++ problems/07-set-time-out.js
@@ -20,6 +20,17 @@ funcTimer(5000, partyFunc); // in 5 seconds prints: "Party time!"
 
 // Your code here
 
+let funcTimer = (time, cb) => {
+  global.setTimeout(cb, time);
+}
+
+function partyFunc() {
+  console.log("Party time!");
+}
+
+funcTimer(5000, partyFunc); // in 5 seconds prints: "Party time!"
+
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
