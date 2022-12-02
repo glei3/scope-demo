97b2905c13ae2dc342cc3fab8613c8346ca857a3
juniper <benklee127@gmail.com>
2022-12-02 16:53:29 -0500

    #04

--- problems/04-arrow-average-value.js
+++ problems/04-arrow-average-value.js
@@ -10,7 +10,15 @@ arrowAvgValue([2, 3, 7]); // => 4
 arrowAvgValue([100, 60, 64]); // => 74.66666666666667
 ***********************************************************************/
 
-// Your code here
+const arrowAvgValue = arr => {
+  let sum = 0;
+  let count = 0;
+  for (let nums of arr) {
+    sum += nums;
+    count++;
+  }
+  return sum / count;
+}
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
